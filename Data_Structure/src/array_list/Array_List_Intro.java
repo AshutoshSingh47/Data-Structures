f69e23ef6fe53f1efa2550b46a50f588c416ad95
AshutoshSingh47 <ashutoshsingh.iimt@gmail.com>
2020-07-17 10:19:23 +0000

    Second Commit

--- Data_Structure/src/array_list/Array_List_Intro.java
+++ Data_Structure/src/array_list/Array_List_Intro.java
@@ -1,19 +1,12 @@
 package array_list;
 import java.util.*;
-//import java.util.LinkedList;
 public class Array_List_Intro {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
 		List<Integer> list=new ArrayList<Integer>();
-		System.out.println("enter the array");
-		while(sc.hasNext()){
-			int i=sc.nextInt();
-			list.add(i);
-		}
-		/*list.add(1);
+		list.add(1);
 		list.add(2);
 		list.add(3);    
-		list.add(4);*/
+		list.add(4);
 		System.out.println(list);	
 	}
 }
