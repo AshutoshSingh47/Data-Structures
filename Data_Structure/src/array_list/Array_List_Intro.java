62dc6173bd549dcb3d947ad8913483d7c4ed14c0
RAJ <RAJ@p-PC>
2020-07-17 22:29:47 +0530

    Sixth commit

--- Data_Structure/src/array_list/Array_List_Intro.java
+++ Data_Structure/src/array_list/Array_List_Intro.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Array_List_Intro {
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s;
-		List<String> list=new LinkedList<String>();
+        int s;
+		List<Integer> list=new LinkedList<Integer>();
 			System.out.println("Enter Elements");
 		for(int i=0;i<5;i++){
-            s=sc.next();
+            s=sc.nextInt();
             list.add(s);
         }
 		System.out.println(list);	
