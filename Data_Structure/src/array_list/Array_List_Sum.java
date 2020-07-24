32b5644298041649cb338ca5107109dbb6a79c43
Ashish Singh <Ashish Singh@DESKTOP-RLMLMJT>
2020-07-24 13:07:22 +0530

    Ashish : Committed on 24/07/2020 at 01:07 P.M.

--- Data_Structure/src/array_list/Array_List_Sum.java
+++ Data_Structure/src/array_list/Array_List_Sum.java
@@ -1,10 +1,26 @@
 package array_list;
-
+import java.util.*;
 public class Array_List_Sum {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+	   List<Integer>list=new ArrayList<Integer>();
+	   int i,j,n,s;s=0;
+	   System.out.println("Enter the size of array list");
+	   n=sc.nextInt();
+	   for(i=0;i<n;i++)
+	   {
+		   j=sc.nextInt();
+		   list.add(j);
+	   }
+	   System.out.println(list);
+	   Iterator itr=list.iterator();
+	   while(itr.hasNext())
+	   {
+		   i=(Integer)itr.next();
+		   s=s+i;
+	   }
+	   System.out.println(s);
+	
 	}
 
 }
