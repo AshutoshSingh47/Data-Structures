060ec606cf0c6bd3436b893115a58927ae21f181
Ashish Singh <Ashish Singh@DESKTOP-RLMLMJT>
2020-07-24 13:35:54 +0530

    Ashish : Committed on 24/07/2020 at 01:35 P.M.

---
+++ Data_Structure/src/array_list/Array_List_Bubble_Sort.java
@@ -0,0 +1,14 @@
+package array_list;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+public class Array_List_Bubble_Sort {
+	public static void main(String[] args) {
+		List<Integer>list= Arrays.asList(1,2,3,4,5,6,7);
+		System.out.println(list);
+		System.out.println(list.size());
+	}
+
+}
