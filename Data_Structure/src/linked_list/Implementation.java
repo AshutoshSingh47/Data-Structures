eb93859163bbe64701bea4bca251e20cf0cf438c
Ashutosh-Tester <muskelon1111@gmail.com>
2020-09-06 23:56:17 +0530

    Batch : AshutoshSingh47 at 11:57 P.M.

---
+++ Data_Structure/src/linked_list/Implementation.java
@@ -0,0 +1,31 @@
+package linked_list;
+
+public class Implementation {
+Node head;
+public void insert(int data){
+	Node node=new Node();
+	node.data=data;
+	node.next=null;
+	if(head==null)
+	{
+		head=node;
+	}
+	else{
+		Node n=head;
+		while(n.next!=null)
+		{
+			n=n.next;
+		}
+		n.next=node;
+	}
+	}
+public void show(){
+	Node node=head;
+	while(node.next!=null){
+		System.out.print(node.data+" ");
+		node=node.next;
+	}
+		System.out.println(node.data);
+}
+}
+
---
+++ Data_Structure/src/linked_list/Node.java
@@ -0,0 +1,11 @@
+package linked_list;
+
+public class Node {
+
+	//public static void main(String[] args) {
+		Node next;
+		int data;
+		
+	}
+
+
