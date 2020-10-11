83ec654ede89ac97a1094e5b43554294cb72ac0a
Ashutosh-Tester <muskelon1111@gmail.com>
2020-10-12 01:02:05 +0530

    nth <LOL> Commit

--- Data_Structure/src/stacks/Input_Output.java
+++ Data_Structure/src/stacks/Input_Output.java
@@ -11,18 +11,20 @@ stack.push(2);
 stack.push(3);
 stack.push(4);
 stack.push(5);
-popped=stack.pop();
-System.out.println(popped);
-int peeked=stack.peek();
-System.out.println(peeked);
-popped=stack.pop();
-System.out.println(popped);
- popped=stack.pop();
-System.out.println(popped);
-popped=stack.pop();
-System.out.println(popped);
-popped=stack.pop();
-System.out.println(popped);
-	}
+System.out.println(stack);
+//popped=stack.pop();
+//System.out.println(popped);
+//int peeked=stack.peek();
+//System.out.println(peeked);
+//popped=stack.pop();
+//System.out.println(popped);
+// popped=stack.pop();
+//System.out.println(popped);
+//popped=stack.pop();
+//System.out.println(popped);
+//popped=stack.pop();
+//System.out.println(popped);
+//	}
 
+}
 }
